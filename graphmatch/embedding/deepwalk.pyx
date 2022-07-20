0afb61d829332b3b682352ba609b0c4e2a2fc3af
Mirko Lenz <mirko@mirkolenz.com>
2022-07-20 15:57:18 +0200

    Update imports in deepwalk

--- graphmatch/embedding/deepwalk.pyx
+++ graphmatch/embedding/deepwalk.pyx
@@ -26,8 +26,8 @@ import psutil
 
 cimport cython
 from ..base cimport Base
-import graph as graph2
-import walks as serialized_walks
+from . import graph as graph2
+from . import walks as serialized_walks
 from .skipgram import Skipgram
 
 
